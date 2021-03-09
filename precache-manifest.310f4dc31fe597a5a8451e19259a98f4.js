fc3d9875dcb61f8eccda1438e272cfe04101fdeb
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2021-03-09 10:16:27 +0100

    Update projects

---
+++ precache-manifest.310f4dc31fe597a5a8451e19259a98f4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee34de18f1d72aa2854",
+    "url": "/css/app.2dd2aa09.css"
+  },
+  {
+    "revision": "f45a1cf8b4c065e3b2aa",
+    "url": "/css/chunk-vendors.36b0096a.css"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc4.28546717.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxK.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "a627bd6bad3a8ec12b5015e8ee5a4d25",
+    "url": "/img/adeyinka.jpg"
+  },
+  {
+    "revision": "0f243cf23562de62bcda14219898728f",
+    "url": "/img/projects/acomart.jpg"
+  },
+  {
+    "revision": "ee032bed8136bb5536c3ca938d3e7823",
+    "url": "/img/projects/aeti.jpg"
+  },
+  {
+    "revision": "307c312f48a0960628191e9b07abb290",
+    "url": "/img/projects/afrocinema.jpg"
+  },
+  {
+    "revision": "f283815c2f5cbc173777bcbb95d759fe",
+    "url": "/img/projects/dreambuildersinter.jpg"
+  },
+  {
+    "revision": "8ce603d684b32b6d5efc3767bac1f2bb",
+    "url": "/img/projects/martadef.jpg"
+  },
+  {
+    "revision": "2a7861465c93e8754e2acb30cfa75fa1",
+    "url": "/img/projects/myafrostream.jpg"
+  },
+  {
+    "revision": "2044cdd6a99f4e63ebf147dd02eb8dc6",
+    "url": "/img/projects/nextlaw.jpg"
+  },
+  {
+    "revision": "cc2a719a6182419cc7506b5209417c48",
+    "url": "/img/projects/payalat.jpg"
+  },
+  {
+    "revision": "9ec22a2083349d02400852b7faac2475",
+    "url": "/img/projects/savycon.jpg"
+  },
+  {
+    "revision": "62b1704a258574c0ef4e27b1c47ceeca",
+    "url": "/img/tools/aws.jpg"
+  },
+  {
+    "revision": "0391780f7267af79c381c52c00369cee",
+    "url": "/img/tools/cPanel.jpg"
+  },
+  {
+    "revision": "8838bcca9c081f441cdfc791419b1274",
+    "url": "/img/tools/flutter.jpg"
+  },
+  {
+    "revision": "790c07354e2a19477c51a6e04e44a23a",
+    "url": "/img/tools/git.jpg"
+  },
+  {
+    "revision": "efa5d0fd5dd669ada62052a0a58c0057",
+    "url": "/img/tools/laravel.jpg"
+  },
+  {
+    "revision": "d30dccf79caedc20982d6f515d035979",
+    "url": "/img/tools/symfony.jpg"
+  },
+  {
+    "revision": "c9c132c51f0fe2f65a3aab35889d0cb2",
+    "url": "/img/tools/vuejs.jpg"
+  },
+  {
+    "revision": "c21e18085a808a928e77c22312219190",
+    "url": "/img/tools/vuetify.jpg"
+  },
+  {
+    "revision": "0003c875cf50fa10a40e1d565e46a5d5",
+    "url": "/img/tools/vultr.jpg"
+  },
+  {
+    "revision": "cec4780db4996758b134a8c2c7c832b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee34de18f1d72aa2854",
+    "url": "/js/app.cf6ccf2b.js"
+  },
+  {
+    "revision": "e4f8893fe0253ca24979",
+    "url": "/js/chunk-712ef5f4.c2db1f18.js"
+  },
+  {
+    "revision": "f45a1cf8b4c065e3b2aa",
+    "url": "/js/chunk-vendors.87d34f51.js"
+  },
+  {
+    "revision": "8f99badc9c49d5343bb5d74e4bc44e86",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
