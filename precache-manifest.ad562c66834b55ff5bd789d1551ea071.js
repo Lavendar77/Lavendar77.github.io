120ecc424087d976bac82d369085b455647a7a57
Adeyink Adefolurin <folurinyinka@gmail.com>
2024-02-12 14:22:43 +0100

    Update profile pic

---
+++ precache-manifest.ad562c66834b55ff5bd789d1551ea071.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e079d75038d6dc16a2",
+    "url": "/css/app.4e6e4dd9.css"
+  },
+  {
+    "revision": "152d6822ad231a998380",
+    "url": "/css/chunk-d99fbf66.f9da3993.css"
+  },
+  {
+    "revision": "fbf8e73a641300f68bb1",
+    "url": "/css/chunk-vendors.499dab64.css"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc4.28546717.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxK.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "c1a02761380a9c16f55d93dad0995201",
+    "url": "/img/adeyinka.jpg"
+  },
+  {
+    "revision": "aa219354f3809b36abe6ec094400d04f",
+    "url": "/img/passion/dev.jpeg"
+  },
+  {
+    "revision": "de6a37ec80bc8af8b8ff6d7bb863381b",
+    "url": "/img/passion/life.jpeg"
+  },
+  {
+    "revision": "f3f151c153bf1d4666e319f859c17864",
+    "url": "/img/passion/mobile.jpeg"
+  },
+  {
+    "revision": "9deba50acf2af3242cb3b3f09517a350",
+    "url": "/img/passion/pm.jpeg"
+  },
+  {
+    "revision": "0f243cf23562de62bcda14219898728f",
+    "url": "/img/projects/acomart.jpg"
+  },
+  {
+    "revision": "ee032bed8136bb5536c3ca938d3e7823",
+    "url": "/img/projects/aeti.jpg"
+  },
+  {
+    "revision": "307c312f48a0960628191e9b07abb290",
+    "url": "/img/projects/afrocinema.jpg"
+  },
+  {
+    "revision": "9f13b6859d4c33435eaf6c9dc239b308",
+    "url": "/img/projects/connectnigeria.jpg"
+  },
+  {
+    "revision": "f283815c2f5cbc173777bcbb95d759fe",
+    "url": "/img/projects/dreambuildersinter.jpg"
+  },
+  {
+    "revision": "f1f8c36c498395ec5f819a7e543f5e84",
+    "url": "/img/projects/gerocare.jpg"
+  },
+  {
+    "revision": "4260f46b3ba994b3d0821b73f4c98a84",
+    "url": "/img/projects/hypetag.jpg"
+  },
+  {
+    "revision": "f2e8d2747f3463c5819cfbdc4fcc76fb",
+    "url": "/img/projects/ksbtech.jpg"
+  },
+  {
+    "revision": "8ce603d684b32b6d5efc3767bac1f2bb",
+    "url": "/img/projects/martadef.jpg"
+  },
+  {
+    "revision": "2a7861465c93e8754e2acb30cfa75fa1",
+    "url": "/img/projects/myafrostream.jpg"
+  },
+  {
+    "revision": "2044cdd6a99f4e63ebf147dd02eb8dc6",
+    "url": "/img/projects/nextlaw.jpg"
+  },
+  {
+    "revision": "313089be24346d03d8e70d5de1e7b107",
+    "url": "/img/projects/ourspace.jpg"
+  },
+  {
+    "revision": "cc2a719a6182419cc7506b5209417c48",
+    "url": "/img/projects/payalat.jpg"
+  },
+  {
+    "revision": "9ec22a2083349d02400852b7faac2475",
+    "url": "/img/projects/savycon.jpg"
+  },
+  {
+    "revision": "da9946c675939be10b5bcb574c455b69",
+    "url": "/img/tools/GCP.jpg"
+  },
+  {
+    "revision": "62b1704a258574c0ef4e27b1c47ceeca",
+    "url": "/img/tools/aws.jpg"
+  },
+  {
+    "revision": "0391780f7267af79c381c52c00369cee",
+    "url": "/img/tools/cPanel.jpg"
+  },
+  {
+    "revision": "7a182268e85c8d9d6dd74ea8e5be059a",
+    "url": "/img/tools/digitalocean.jpg"
+  },
+  {
+    "revision": "8838bcca9c081f441cdfc791419b1274",
+    "url": "/img/tools/flutter.jpg"
+  },
+  {
+    "revision": "790c07354e2a19477c51a6e04e44a23a",
+    "url": "/img/tools/git.jpg"
+  },
+  {
+    "revision": "efa5d0fd5dd669ada62052a0a58c0057",
+    "url": "/img/tools/laravel.jpg"
+  },
+  {
+    "revision": "869b8eb0da1ff88bf90e1d2e98a499be",
+    "url": "/img/tools/meilisearch.jpg"
+  },
+  {
+    "revision": "d30dccf79caedc20982d6f515d035979",
+    "url": "/img/tools/symfony.jpg"
+  },
+  {
+    "revision": "c9c132c51f0fe2f65a3aab35889d0cb2",
+    "url": "/img/tools/vuejs.jpg"
+  },
+  {
+    "revision": "c21e18085a808a928e77c22312219190",
+    "url": "/img/tools/vuetify.jpg"
+  },
+  {
+    "revision": "0003c875cf50fa10a40e1d565e46a5d5",
+    "url": "/img/tools/vultr.jpg"
+  },
+  {
+    "revision": "b6c208739a408c86931b255049605487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e079d75038d6dc16a2",
+    "url": "/js/app.818d0060.js"
+  },
+  {
+    "revision": "152d6822ad231a998380",
+    "url": "/js/chunk-d99fbf66.abccb0f9.js"
+  },
+  {
+    "revision": "fbf8e73a641300f68bb1",
+    "url": "/js/chunk-vendors.12a696c5.js"
+  },
+  {
+    "revision": "8f99badc9c49d5343bb5d74e4bc44e86",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.ad562c66834b55ff5bd789d1551ea071.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "lavendar77"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
